39b7a5d05efa0e4ae4261c1a0c7aae4274a1ef83
Michel Oliveira <michel.oliveira999@hotmail.com>
2022-03-04 20:49:31 -0300

    Adding error handling to methods

--- back-end/src/graphql/Users/resolvers.js
+++ back-end/src/graphql/Users/resolvers.js
@@ -2,27 +2,56 @@ const UserRepository = require('../../repositories/UserRepository');
 
 module.exports = {
   Query: {
-    listUsers: async() => await UserRepository.findAll(),
-    findUserById: async(_, { id }) => await UserRepository.findUserById(Number(id)),
+    listUsers: async () => {
+      try {
+        return await UserRepository.findAll();
+      } catch (err) {
+        console.log(err);
+      }
+    },
+
+    findUserById: async (_, { id }) => {
+      try {
+        if (id) {
+          return await UserRepository.findUserById(Number(id));
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
   },
 
   Mutation: {
-    createUser: async(_, { data }) => {
-      const { firstName, email, password } = data;
-
-      if([firstName, email, password].some((item) => !item)) {
-        return 'There are empty required fields';
-      }
-
-      const newUser = await UserRepository.create(data);
-      return newUser;
+    createUser: async (_, { data }) => {
+      try {
+        const { firstName, email, password } = data;
+  
+        if([firstName, email, password].some((item) => !item)) {
+          return 'There are empty required fields';
+        }
+  
+        const newUser = await UserRepository.create(data);
+        return newUser;
+      } catch (err) {
+        console.log(err);
+      };
     },
 
-    updateUser: async(_, { id, data }) => {
-      const updatedUser = await UserRepository.update(Number(id), data);
-      return updatedUser;
+    updateUser: async (_, { id, data }) => {
+      try {
+        const updatedUser = await UserRepository.update(Number(id), data);
+        return updatedUser;
+      } catch (err) {
+        console.log(err);
+      };
     },
 
-    deleteUser: async(_, { id }) => await UserRepository.delete(Number(id)),
+    deleteUser: async (_, { id }) => {
+      try {
+        return await UserRepository.delete(Number(id))
+      } catch (err) {
+        console.log(err);
+      };
+    },
   }
-}
+};
--- back-end/src/repositories/UserRepository.js
+++ back-end/src/repositories/UserRepository.js
@@ -2,51 +2,76 @@ let users = require('../mock/mock_users');
 
 class UserRepository {
   findAll() {
-    return new Promise((resolve) => resolve(users));
-  }
+    return new Promise((resolve, reject) => {
+      const listOfUsers = users;
+
+      if (listOfUsers) {
+        resolve(listOfUsers);
+      } else {
+        reject({err: 'Error fetching users'});
+      };
+    });
+  };
 
   findUserById(id) {
-    return new Promise((resolve) => resolve(
-      users.find((user) => user.id === id)
-    ))
-  }
+    return new Promise((resolve, reject) => {
+      if (id) {
+        resolve(users.find((user) => user.id === id))
+      } else {
+        reject({ err: 'Error fetching user' });
+      };
+    });
+  };
 
   create(data) {
-    return new Promise((resolve) => {
-      const newUser = {
-        id: users.length + 1,
-        ...data,
-      }
-
-      users.push(newUser);
-      resolve(newUser);
+    return new Promise((resolve, reject) => {
+      if (data) {
+        const newUser = {
+          id: users.length + 1,
+          ...data,
+        };
+  
+        users.push(newUser);
+        resolve(newUser);
+      } else {
+        reject({ err: 'Error creating user' });
+      };
     });
-  }
+  };
 
   update(id, data) {
-    return new Promise((resolve) => {
-      let updatedUser = {};
-
-      users.forEach((user, index) => {
-        if(user.id === id) {
-          updatedUser = {
-            ...user,
-            ...data
+    return new Promise((resolve, reject) => {
+      if (id && data) {
+        let updatedUser = {};
+  
+        users.forEach((user, index) => {
+          if(user.id === id) {
+            updatedUser = {
+              ...user,
+              ...data
+            };
+            users[index] = updatedUser;
           };
-          users[index] = updatedUser;
-        }
-      })
-      resolve(updatedUser);
-    })
-  }
+        });
+        
+        resolve(updatedUser);
+      } else {
+        reject({ err: 'Error updating user' });
+      };
+    });
+  };
 
   delete(id) {
     const deleted = true;
-    return new Promise((resolve) => {
-      users = users.filter((user) => user.id !== id);
-      resolve(deleted);
+    return new Promise((resolve, reject) => {
+      if (id) {
+        users = users.filter((user) => user.id !== id);
+        resolve(deleted);
+      } else {
+        reject({ err: 'Error deleting user' });
+      };
     });
-  }
-}
+  };
+};
 
 module.exports = new UserRepository();
