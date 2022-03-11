703d64a1f4b126cacd4f667b546b47b56a11762d
Michel Oliveira <michel.oliveira999@hotmail.com>
2022-03-11 20:37:36 -0300

    Adding error handling to post methods

--- back-end/src/graphql/Posts/resolvers.js
+++ back-end/src/graphql/Posts/resolvers.js
@@ -3,21 +3,35 @@ const UserRepository = require("../../repositories/UserRepository");
 
 module.exports = {
   Query: {
-    posts: async() => PostRepository.listPost(),
+    posts: async () => {
+      try {
+        return await PostRepository.listPost();
+      } catch (err) {
+        console.log(err);
+      };
+    },
   },
 
   Mutation: {
-    createPost: async(_, { data }) => {
-      const { authorId } = data;
-      const author = UserRepository.findUserById(Number(authorId));
-
-      const newPost = PostRepository.create(data, author);
-      return newPost;
+    createPost: async (_, { data }) => {
+      try {
+        const { authorId } = data;
+        const author = UserRepository.findUserById(Number(authorId));
+  
+        const newPost = PostRepository.create(data, author);
+        return newPost;
+      } catch (err) {
+        console.log(err);
+      };
     },
 
-    deletePost: async(_, { id }) => {
-      const postDeleted = PostRepository.delete(Number(id));
-      return postDeleted;
+    deletePost: async (_, { id }) => {
+      try {
+        const postDeleted = PostRepository.delete(Number(id));
+        return postDeleted;
+      } catch (err) {
+        console.log(err);
+      };
     },
   },
 };
