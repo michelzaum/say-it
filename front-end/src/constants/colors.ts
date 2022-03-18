4d2fb613ded5765cf1ee1ad12e23c4fb80c95ec7
Michel Oliveira <michel.oliveira999@hotmail.com>
2022-03-18 20:04:30 -0300

    Creating the first constant files

---
+++ front-end/src/constants/colors.ts
@@ -0,0 +1,6 @@
+export const colors = {
+  primaryBlue: '#2653E8',
+  white: '#FAFAFA',
+  gray: '#B1B1B1',
+  dark: '#000537',
+};
---
+++ front-end/src/constants/measures.ts
@@ -0,0 +1,7 @@
+export const measures = {
+  'spacing-small': 8,
+  'spacing-medium': 16,
+  'spacing-large': 24,
+  'spacing-x-large': 32,
+  'spacing-xx-large': 40,
+};
