24d8f5cea927df6ad22f46003d116aac602650d0
Michel Oliveira <michel.oliveira999@hotmail.com>
2022-04-02 01:04:16 -0300

    Fixing bug in "returnRandomPeople" function, which returned undefined data

--- front-end/src/utils/returnRandomPeople.ts
+++ front-end/src/utils/returnRandomPeople.ts
@@ -2,7 +2,7 @@ import { randomPeople } from '../mock/randomPeople';
 
 export function returnRandomPeople () {
   const min = 0;
-  const max = randomPeople.length + 1;
+  const max = randomPeople.length;
   const randomIndex = Math.floor(Math.random() * (max - min)) + min;
 
   return randomPeople[randomIndex];
