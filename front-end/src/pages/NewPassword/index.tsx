761a9a7c037bdfc00447f61d6a7770fbb52274e2
Michel Oliveira <michel.oliveira999@hotmail.com>
2022-04-08 17:42:26 -0300

    Getting the user id in the New Password page

--- front-end/src/pages/NewPassword/index.tsx
+++ front-end/src/pages/NewPassword/index.tsx
@@ -1,6 +1,14 @@
+import { useLocation } from 'react-router-dom';
+
 import { NewPasswordContainer } from './styles';
 
 export const NewPassword = () => {
+  const location = useLocation();
+
+  const params = location.state;
+
+  console.log(params);
+
   return (
     <NewPasswordContainer>
       <h1>New Password</h1>
